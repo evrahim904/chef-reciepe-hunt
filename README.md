7924f1c9b0b560dd465240bf4996130e443dba1a
unknown <you@example.com>
2023-05-05 20:20:58 +0600

    blog data has been showed

---
+++ README.md
@@ -0,0 +1,6 @@
+### i have added a navbar and loaded my all chefs data 
+### i have created login and register pages
+### i have also created a redirect page w hen i click on the view recepies page i will be redirected on user details page
+### i have creates spinner 404 page and  eviroment variable firebase config
+### i have displayed all the iformation
+###
--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import Footer from './Footer';
-import 'react-tooltip/dist/react-tooltip.css'
-import { Tooltip } from 'react-tooltip'
-
 const Blog = () => {
     return (
         <section>
-         <a className="my-anchor-element" data-tooltip-content="ibrahim!">
-  mmmmmmmmmmmmmmmmmmmmmm
-</a>
-
-<Tooltip anchorSelect=".my-anchor-element" />
-            blog is comming
+        <div style={{marginBottom:'280px'}}>
+            <h3> differences between uncontrolled and controlled components?</h3>
+            <p><strong>answer:controlled components refer to components that have their state and behavior controlled by the parent component. These components rely on props passed down from the parent component to update their state and behavior. Uncontrolled components refer to components that manage their own state internally.</strong></p>
+            <h3>How to validate React props using PropTypes?</h3>
+            <p><strong>answer:With React props, you can send data to a component when you call on that component, including numbers, strings, functions, objects, and arrays. If you have multiple components, you can pass data from one component to another.</strong></p>
+            <h3>difference between nodejs and express js</h3>
+            <p><strong>answer:NodeJS is an event-driven, non-blocking I/O model using JavaScript as its main language. It helps to build scalable network applications. Express is a minimal and flexible Node. js web application framework that provides a robust set of features for web and mobile applications.</strong></p>
+            <h3>What is a custom hook, and why will you create a custom hook?</h3>
+            <p><strong>answer:Custom React JS hooks offer reusability as when a custom hook is created, it can be reused easily, which makes the code cleaner and reduces the time to write the code. It also enhances the rendering speed of the code as a custom hook does not need to be rendered again and again while rendering the whole code.</strong></p>
+        </div>
             <Footer></Footer>
         </section>
        
